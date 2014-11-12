e4518e30f59b12a201f72b8d0c53144fc571323f
Frank Schlimbach <frank.schlimbach@intel.com>
2014-11-12 19:17:34 +0100

    let travis use make_kit.py

--- make_kit.py
+++ make_kit.py
@@ -5,9 +5,10 @@ release = "current"
 ARCHS = ['intel64']
 devbuild = False
 keepbuild = False
+buildonly = False
 
 try:
-  opts, args = getopt.getopt(sys.argv[1:],"dkh",["devbuild","keep","help"])
+  opts, args = getopt.getopt(sys.argv[1:],"bdkh",["buildonly", "devbuild","keep","help"])
 except getopt.GetoptError:
   print 'make_kit.py [-d] [-k] [-h]'
   sys.exit(2)
@@ -18,6 +19,8 @@ for opt, arg in opts:
   elif opt in ("-d", "--devbuild"):
     devbuild = True
   elif opt in ("-k", "--keep"):
+    keepbuild = True
+  elif opt in ("-b", "--buildonly"):
     keepbuild = True
 
 
@@ -72,12 +75,15 @@ for vs in VSS:
         for rel in ['Release','Debug']:
             builddir = 'kit.' + rel
             if keepbuild == False:
-              shutil.rmtree(builddir, True )
-              os.mkdir(builddir)
+                util.rmtree(builddir, True )
+            if os.path.isdir(builddir) == False:
+                os.mkdir(builddir)
+
             exe_cmd('cd ' + builddir + '; cmake -DCMAKE_BUILD_TYPE=' + rel
                     + ' -DBUILD_LIBS_FOR_MPI=TRUE -DBUILD_LIBS_FOR_ITAC=TRUE -DCNC_PRODUCT_BUILD=TRUE -DTBBROOT=' + tbbroot
                     + ' -DCMAKE_INSTALL_PREFIX=' + os.path.join('..', reldir) + ' .. && make -j 16 install')
         
-        exe_cmd( 'chmod 644 `find ' + reldir + ' -type f` && chmod 755 `find ' + reldir + ' -name \*sh`' )
-        exe_cmd( 'dos2unix -q `find ' + reldir +' -name \*.h` `find ' + reldir +' -name \*sh`') # && dos2unix -q `find ' + reldir +' -name \*txt` && dos2unix -q `find ' + reldir +' -name \*cpp`)
-        exe_cmd('cd ' + kitdir + ' && tar cfvj cnc_' + release + '_pkg.tbz cnc')
+        if buildonly == False:
+            exe_cmd( 'chmod 644 `find ' + reldir + ' -type f` && chmod 755 `find ' + reldir + ' -name \*sh`' )
+            exe_cmd( 'dos2unix -q `find ' + reldir +' -name \*.h` `find ' + reldir +' -name \*sh`') # && dos2unix -q `find ' + reldir +' -name \*txt` && dos2unix -q `find ' + reldir +' -name \*cpp`)
+            exe_cmd('cd ' + kitdir + ' && tar cfvj cnc_' + release + '_pkg.tbz cnc')
