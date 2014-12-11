0870f104a5e3aa4f2aa968505ab01b4cb50b92f0
Christoph Junghans <junghans@lanl.gov>
2014-12-10 18:50:11 -0700

    cmake: fixed install location for share data
    * samples, apidoc and misc go now to into PREFIX/share/icnc
    * don't mark bat files executable on non-Windows installs

--- tests/run.py
+++ tests/run.py
@@ -2,7 +2,7 @@ import sys
 import os
 launcher={ 'shared' : 'env ' + sys.argv[4],
            'MPI'    : 'mpirun -genv ' + sys.argv[4] + ' -genv DIST_CNC=MPI -n 4',
-           'SOCKETS': 'env ' + sys.argv[4] + ' DIST_CNC=SOCKETS CNC_SOCKET_HOST=$CNCROOT/misc/distributed/socket/start_batch.sh',
+           'SOCKETS': 'env ' + sys.argv[4] + ' DIST_CNC=SOCKETS CNC_SOCKET_HOST=$CNCROOT/share/icnc/misc/distributed/socket/start_batch.sh',
            'SHMEM'  : 'env ' + sys.argv[4] + ' DIST_CNC=SHMEM'
 }
 cmd=launcher[sys.argv[2]] + " " + sys.argv[1] + " " + " ".join(sys.argv[5:]) + " > " + sys.argv[3]
